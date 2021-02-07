d96763aad9551546415c7e840420600a4ffe8999
Mayokara <62886973+Mayokara@users.noreply.github.com>
2021-02-08 01:21:56 +0900

    Update 081_B.py

--- ABC081_B.py
+++ ABC081_B.py
@@ -1,14 +1,16 @@
 # ABC081_B
 
 n = int(input())
-
 A = list(map(int, input().split()))
 
 # 各要素が2で割り切れる回数をリスト化 → 最小値表示
+cnt = [0]*len(A)
+for i in range(len(A)):
+    a = A[i]
+    while a % 2 == 0:
+        cnt[i] += 1
+        a = a // 2    
 
-
-
-
-
+print(min(cnt))
 
 # 割り切れないものが出たところでやめる
