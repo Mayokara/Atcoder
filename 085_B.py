dc71493f7901b230826f66f3fd30b50f3f676a51
Mayokara <62886973+Mayokara@users.noreply.github.com>
2021-02-08 17:55:25 +0900

    Add 085_B.py

---
+++ 085_B.py
@@ -0,0 +1,17 @@
+
+n = int(input())
+list_d = []
+# n個の直径の入力をリストにして、降順にソート
+for i in range(n):
+    list_d.append(int(input()))
+list_d.sort(reverse=True)
+
+# 1つ前の要素より大きかったらres += 1
+res = 1
+for i in range(1, n):
+    if list_d[i] < list_d[i-1]:
+        res += 1
+
+print(res)
+
+#list_dをset型に変換して重複要素をなくしてもいいかも
