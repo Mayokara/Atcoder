7cdf654de948adb2db787d0576a314aaa62e7a9f
Mayokara <62886973+Mayokara@users.noreply.github.com>
2021-02-08 16:27:52 +0900

    Add 083_B.py :zap:

---
+++ 083_B.py
@@ -0,0 +1,18 @@
+# 各桁の和を計算
+def sum_digit(num):
+    t = 0
+    while num > 0:
+        t += num % 10
+        num //= 10
+    return t
+
+n, a, b = map(int, input().split())
+
+# 数字の各桁の和がa以上b以下のとき、resにその数字を足していく
+res = 0
+for i in range(1, n + 1):
+    temp = sum_digit(i)
+    if a <= temp <= b:
+        res += i
+    
+print(res)
