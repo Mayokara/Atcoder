48e20baf4d4bbf3ae0f8b1b33ccfa4c87d2221c1
Mayokara <62886973+Mayokara@users.noreply.github.com>
2021-02-21 20:05:24 +0900

    Update files :sparkles:

--- ABC192/D.py
+++ ABC192/D.py
@@ -1,4 +1,4 @@
-# 10進数をn進数に変換
+# n進数表記の数を10進数に変換
 # 二分探索
 
 x = input()
@@ -8,22 +8,38 @@ m = int(input())
 x_list = sorted([int(i) for i in x], reverse=True)
 d = x_list[0]
 
-n = d + 1
-cnt = 0
+# n進法表記から10進法にする関数（int(x, n)だと2<=n<=36までしか対応せず）
+def convert(base):
+    deci = 0
+    for i, e in enumerate(x[::-1]): # [::-1]　全ての要素を逆向きに得る
+        deci += int(e) * (base ** i)
+    return deci
 
 if len(x) == 1: # 反省：xが一文字のときの場合分けできておらず　
-    if int(x, n) <= m:
-        cnt = 1
+    if int(x) <= m:
+        print(1)
     else:
-        cnt = 0
-    print(cnt)
+        print(0)
     exit()
 
-else:
-    while int(x, n) <= m:
-        n += 1
-        cnt += 1
+# else:
+#     while int(x, n) <= m:
+#         n += 1
+#         cnt += 1
 
-    print(cnt)
+#     print(cnt)
+
+################################################################
+
+else:
+    lower = d
+    upper = 2 * (10 ** 18)
+    while upper - lower > 1:
+        mid = (lower + upper) // 2
+        if convert(mid) <= m:
+            lower = mid
+        else:
+            upper = mid
+    print(lower - d)
 
 
--- 過去問精選/087_C.py
+++ 過去問精選/087_C.py
@@ -2,21 +2,29 @@
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(2)]
 
-total = [0 for _ in range(n)]
+# total = [0 for _ in range(n)]
 
-ans = -1
-for i in range(n):
-    total[i] = sum(a[0][:(i + 1)]) + sum(a[1][i:])
-    if ans < total[i]:
-        ans = total[i]
+# ans = -1
+# for i in range(n):
+#     total[i] = sum(a[0][:(i + 1)]) + sum(a[1][i:])
+#     if ans < total[i]:
+#         ans = total[i]
 
-print(ans)
+# print(ans)
 
 cum_0 = a[0][0]
 cum_1 = a[0][0] + a[1][0]
+
 for i in range(1, n):
     cum_0 += a[0][i]
-    if a[]
+    if a[0][i] >= a[1][i - 1]:
+        cum_1 = cum_0 + a[1][i]
+    else:
+        cum_1 += a[1][i]
+
+print(cum_1)
+
+
 
 
 
